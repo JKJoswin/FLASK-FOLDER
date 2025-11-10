1b033bdd2a17a806e3c87018be2f2fc86dceb80b
Joswin <joswinjayakumar@gmail.com>
2025-11-10 18:01:24 +0530

    Flask Class-6

--- my_flask_app_5_login_app/app.py
+++ my_flask_app_5_login_app/app.py
@@ -1,33 +1,72 @@
-from flask import Flask,render_template,request
-import requests
+from flask import Flask,render_template,request,redirect,url_for,session,flash
+import sqlite3
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
+app.secret_key='your_secret_key'
+def init_db():
+    conn=sqlite3.connect('users.db')
+    c=conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXIST users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            username TEXT UNIQUE NOT NULL
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-API_KEY="a74c770384b8064cd1a807d8a5e51842"
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-
-@app.route("/")
-
+@app.route('/')
 def home():
-    return render_template("index.html",weather=None,error=None)
+    if 'username' in session:
+        return render_template('home.html',username=session['username'])
+    return redirect(url_for('login'))
 
-@app.route("/weather",methods=['POST'])
-def weather():
-    city=request.form["city"]
-    url=f"{BASE_URL}q={city}&appid={API_KEY}&units=metric"
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        username=request.form['username']
+        password=request.form['password']
 
-    response=requests.get(url)
-    data=response.json()
+        hashed_pw=generate_password_hash(password)
 
-    if data['cod']!="404":
-        weather_data={
-            "country_code":data["sys"]["country"],
-            "temperature":data["main"]["temp"],
-            "location":data["name"]
-        }
-        return render_template("index.html",weather=weather_data,error=None)
-    else:
-        return render_template("index.html",weather=None,error="City not found!")
+        try:
+            conn=sqlite3.connect('users.db')
+            c=conn.cursor()
+            c.execute("INSERT INTO users (username,password) VALUES (?,?)",(username,hashed_pw))
+            conn.commit()
+            conn.close()
+            flash("Signup Successful! You can Login Now!","SUCCESS!")
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash("Username already exists! Try a different one!","ERROR!")
+        
+        return render_template('signup.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username=request.form['username']
+        password=request.form['password']
+
+        conn=sqlite3.connect('users.db')
+        c=conn.cursor()
+        c.execute("SELECT password FROM users WHERE username=?",(username,))
+        user=c.fetchone()
+        conn.close()
+
+        if user and check_password_hash(user[0],password):
+            session['username']=username
+            flash("Login Successful!","SUCCESS!")
+            return redirect(url_for(home))
+        else:
+            flash("Invalid Username or Password!","ERROR!")
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    flash("Logged out Successfully!","INFO!")
+    return redirect(url_for('login'))
