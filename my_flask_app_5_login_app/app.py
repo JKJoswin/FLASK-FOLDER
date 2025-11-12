5e192afd5650a9c7646115e133abf6708d2c025a
Joswin <joswinjayakumar@gmail.com>
2025-11-12 17:36:22 +0530

    Flask Class-7

--- my_flask_app_5_login_app/app.py
+++ my_flask_app_5_login_app/app.py
@@ -3,14 +3,13 @@ import sqlite3
 from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
-app.secret_key='your_secret_key'
 def init_db():
     conn=sqlite3.connect('users.db')
     c=conn.cursor()
     c.execute('''
-        CREATE TABLE IF NOT EXIST users(
-            id INTEGER PRIMARY KEY AUTOINCREMENT
-            username TEXT UNIQUE NOT NULL
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL
         )
     ''')
@@ -70,3 +69,7 @@ def logout():
     session.pop('username',None)
     flash("Logged out Successfully!","INFO!")
     return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
